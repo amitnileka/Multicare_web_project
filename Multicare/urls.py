bf3abf408cdfb664ed6d42d4afa098c3c6afedbf
Amit Nilekar <53910859+amitnileka@users.noreply.github.com>
2021-04-25 12:13:33 +0530

    Update urls.py

--- Multicare/urls.py
+++ Multicare/urls.py
@@ -18,6 +18,8 @@ from django.urls import path
 from Hosp.views import *
 from Hosp import views
 from django.contrib.auth import views as auth_views
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 urlpatterns = [
@@ -63,4 +65,4 @@ path('password/',PasswordChangeView.as_view(),name='change_password'),
     path('reset/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(),name='password_reset_confirm'),
 
     path('reset/done/',auth_views.PasswordResetCompleteView.as_view(),name='password_reset_complete'),
-    ]
+    ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
